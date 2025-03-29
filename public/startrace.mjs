3aa4d15bfb2f87a32846895173b70eaf79541ed0
ojnode <ojinnakabu95@gmail.com>
2025-03-29 20:19:20 +0000

    I think I  moved the custom clock to a diff a file

--- public/startrace.mjs
+++ public/startrace.mjs
@@ -1,102 +1,36 @@
+import * as util from './utils.js';
+
 const em = {};
 
 function prepareHandles() {
-    em.timer = document.querySelector('#timer');
+    em.timerSection = document.querySelector('#center');
+    const timeButtons = em.timerSection.querySelectorAll('[id]');
+    debugger;
+    for (const button of timeButtons) {
+        em[button.id] = button;
+    } 
 }
 
-const template = document.createElement('template');
-template.innerHTML = `<div>
-    <h4>
-        <div id="marathonclock">
-                <div id='hour'></div> :
-                <div id='min'></div> :
-                <div id='sec'></div>
-        </div>
-    </h4>
-</div>
-<style>
-    #marathonclock {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        color: red;
-        font-size: 3rem;
-        gap: 2px;
-    }
-</style>`
+function startRace() {
+    em.timer.stopTimer();
+    em.timer.startTimer();
+}
 
-class Timer extends HTMLElement {
-    constructor() {
-        super();
-        this.handles = {};
-        this.intervalid;
-        this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
+function stopTimer() {
+    em.timer.stopTimer();
+    em.timer.currentTime();
+}
 
-    prepareHandles() {
-        this.handles.sec = this.shadowRoot.querySelector('#sec');
-        this.handles.min = this.shadowRoot.querySelector('#min');
-        this.handles.hour = this.shadowRoot.querySelector('#hour');
-    }
-
-    timeUnits(handles, timeout) {
-        setTimeout(() => {
-            clearInterval(this.intervalid);
-            this.intervalid = setInterval(() => {
-                handles.innerText++;
-            }, 1000)
-        }, timeout)
-    }
-
-    startTimer() {
-        this.prepareHandles();
-        this.handles.sec.innerText = 0;
-        this.handles.min.innerText = 0;
-        this.handles.hour.innerText = 0;
-        this.timeUnits(this.handles.sec, 0);
-        this.timeUnits(this.handles.min, 4000);
-        this.timeUnits(this.handles.hour, 9000);
-    }
-
-    stopTimer() {
-        clearInterval(this.intervalid);
-    }
-
+function finishTime() {
+    const lap = em.timer.currentTime();
+    console.log(lap);
 }
 
 function load() {
     prepareHandles();
-    startTimer();
-    stopTimer();
+    util.setupButtons(em.startTimer, startRace);
+    util.setupButtons(em.stopTimer, stopTimer);
+    util.setupButtons(em.lap, finishTime);
 }
 
-// function to restart marathon timer
-function startTimer() {
-    em.timer.startTimer();
-    em.restart = document.querySelector('#restart');
-    em.restart.addEventListener("touchstart", () => {
-        em.restart.classList.toggle('active');
-    });
-
-    em.restart.addEventListener("touchend", () => {
-        em.restart.classList.toggle('active');
-        em.timer.stopTimer();
-        em.timer.startTimer();
-    });
-}
-
-// function to stop marathon timer
-function stopTimer() {
-    em.stop = document.querySelector('#stop');
-    em.stop.addEventListener("touchstart", () => {
-        em.stop.classList.toggle('active');
-    });
-
-    em.stop.addEventListener("touchend", () => {
-        em.stop.classList.toggle('active');
-        em.timer.stopTimer();
-    });
-}
-
-window.customElements.define('custom-timer', Timer);
 document.addEventListener("DOMContentLoaded", load);
