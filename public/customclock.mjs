3e14f9de61068539143daa38243b46a2a46990ca
ojnode <ojinnakabu95@gmail.com>
2025-04-02 04:49:10 +0100

    change file extension

--- public/customclock.mjs
+++ public/customclock.mjs
@@ -85,31 +85,4 @@ export class Timer extends HTMLElement {
     }
 }
 
-const template = document.createElement('template');
-template.innerHTML = `
-<div>
-    <h4 style= "color: red"></h4>
-</div>`
-
-class Savedrunner extends HTMLElement {
-    constructor() {
-        super();
-        
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-
-    set name(value) {
-        this.shadowRoot.querySelector('h4').innerText = 
-        `${value} saved`;
-    }
-
-    set finishLine(value) {
-        this.shadowRoot.querySelector('h4').innerText = 
-        `${value} reached the finish line`;
-    }
-}
-
-window.customElements.define('saved-runner', Savedrunner);
-
 window.customElements.define('custom-timer', Timer);
