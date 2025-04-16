38563bb23613ddb36cd7020a8077fefa857340cd
ojnode <ojinnakabu95@gmail.com>
2025-04-16 21:37:47 +0100

    leaderboard function

--- public/startrace.mjs
+++ public/startrace.mjs
@@ -1,18 +1,24 @@
-import * as util from './utils.js';
+import * as util from './utils.mjs';
 
 const em = {};
 
 function prepareHandles() {
     em.timerSection = document.querySelector('#center');
     const timeButtons = em.timerSection.querySelectorAll('[id]');
-    debugger;
     for (const button of timeButtons) {
         em[button.id] = button;
     } 
 }
 
+function setupEventListerners() {
+    util.setupButtons(em.startTimer, startRace);
+    util.setupButtons(em.stopTimer, stopTimer);
+    util.setupButtons(em.lap, finishTime);
+    util.setupButtons(completed, leaderBoard);
+}
+
 export function finishTime() {
-    const lap =  em.timer.currentTime();
+    return em.timer.currentTime();
 }
 
 function startRace() {
@@ -25,20 +31,28 @@ function stopTimer() {
     em.timer.currentTime();
 }
 
-function setupEventListerners() {
-    util.setupButtons(em.startTimer, startRace);
-    util.setupButtons(em.stopTimer, stopTimer);
-    util.setupButtons(em.lap, finishTime);
-    util.setupButtons(completed, completeRunner);
+async function retrieveRunner() {
+    const getSavedRunner = await fetch(`runner/${em.playerID.value}`);
+    const value = await getSavedRunner.json();
+    return value;
 }
 
 // record finish time
-async function completeRunner() {
-    em.url = `runner/${em.playerID.value}`
-    const response = await fetch(em.url);
-    const value = await response.json()
-    em.saveplayer.finishLine = value.runnerName;
+async function leaderBoard() {
+    const value = await retrieveRunner();
+    const method = 'POST';
+    const headers = { 'Content-Type': 'application/json' };
+    const body = JSON.stringify({
+        runnerId: value.runner_id,
+        positionId: em.positionID.value
+    });
+    const options = { method, headers, body }
+
+    const response = await fetch('leaderBoard', options);
+    const {runnerName, position_id} = await response.json();
+    saveplayer.setFinishLine(runnerName, position_id);
 }
+
 
 function load() {
     prepareHandles();
