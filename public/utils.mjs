6351bbf7f2ea782d058fb21ed51fa354fc6b86db
ojnode <ojinnakabu95@gmail.com>
2025-05-03 12:02:32 +0100

    move func to touchstart event

--- public/utils.mjs
+++ public/utils.mjs
@@ -2,9 +2,9 @@
 export function setupButtons(elem, func) {
     elem.addEventListener("touchstart", () => {
         elem.classList.toggle('active');
+        func();
     });
     elem.addEventListener("touchend", () => {
         elem.classList.toggle('active');
-        func();
     });
 }
