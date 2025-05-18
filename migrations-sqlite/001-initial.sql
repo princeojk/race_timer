381b8977c35a642cdf8b016c584e041ec61458e1
ojnode <ojinnakabu95@gmail.com>
2025-05-18 16:27:35 +0100

    add race table to enable user save different races

--- migrations-sqlite/001-initial.sql
+++ migrations-sqlite/001-initial.sql
@@ -1,13 +1,20 @@
 --Up
-CREATE TABLE Runners (
+CREATE TABLE race (
+    race_id INTEGER PRIMARY KEY,
+    race_name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE runners (
     runner_id INTEGER PRIMARY KEY,
     runnerName TEXT NOT NULL,
     position_id INTEGER,
-    finish_time TEXT
+    finish_time TEXT,
+    race_id INTEGER,
+    FOREIGN KEY (race_id)
+        REFERENCES race (race_id)
 );
 
-INSERT INTO Runners (runnerName) VALUES
-('John Paul');
+INSERT INTO race (race_name) VALUES ('Portsmouth marathon');
 
 -- Down
 DROP TABLE Runners;
